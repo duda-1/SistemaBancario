58f6c0bc50a5ed3db3562c7dfef86fbe697225ac
Alunos <eduardacristinasilva2008@gmail.com>
2024-11-18 17:21:24 -0300

    18/11

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SistemaBancario;
+using System.Drawing;
+
+double Saldo = 500;
+double LimiteCredito = 200;
+double valor;
+double TaxaJuros = 0.5;
+
+Console.WriteLine("Valor Que deseja Depositar:");
+valor = double.Parse(Console.ReadLine());
+Saldo =  valor + (Saldo * TaxaJuros);
+Console.WriteLine($"Valor Depositado com sucesso!!\n Saldo Atual: {Saldo}");
+
+Console.WriteLine("Quantos deseja Sacar?");
+valor = double.Parse(Console.ReadLine());
+
+if (valor > 0 && valor <= Saldo+LimiteCredito)
+{
+    Saldo = Saldo - valor;
+    Console.WriteLine($"Valor retirado com sucesso!! Saldo Atual {Saldo}");
+
+}
+else
+{
+    Console.WriteLine("Saldo Insuficiente");
+
+}
+
+
+
