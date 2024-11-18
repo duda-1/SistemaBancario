9b714f0d8bc5b8e297f8a702f0bef81c770be093
Alunos <eduardacristinasilva2008@gmail.com>
2024-11-18 17:21:56 -0300

    p

--- Program.cs
+++ Program.cs
@@ -1,30 +1,6 @@
 ﻿using SistemaBancario;
 using System.Drawing;
 
-double Saldo = 500;
-double LimiteCredito = 200;
-double valor;
-double TaxaJuros = 0.5;
-
-Console.WriteLine("Valor Que deseja Depositar:");
-valor = double.Parse(Console.ReadLine());
-Saldo =  valor + (Saldo * TaxaJuros);
-Console.WriteLine($"Valor Depositado com sucesso!!\n Saldo Atual: {Saldo}");
-
-Console.WriteLine("Quantos deseja Sacar?");
-valor = double.Parse(Console.ReadLine());
-
-if (valor > 0 && valor <= Saldo+LimiteCredito)
-{
-    Saldo = Saldo - valor;
-    Console.WriteLine($"Valor retirado com sucesso!! Saldo Atual {Saldo}");
-
-}
-else
-{
-    Console.WriteLine("Saldo Insuficiente");
-
-}
-
-
+ContaCorrente pessoa1= new ContaCorrente { Titular="Teste", Saldo=500,LimiteCredito=300};
+ContaPoupanca pessoa2= new ContaPoupanca { Titular="Teste2", Saldo=500, TaxaJuros=0.5};
 
